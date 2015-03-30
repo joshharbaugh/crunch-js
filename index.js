5dd86da3e24e80482b9733f0d6283089ca2a049b
Enrique Alcantara <enriquealc27@gmail.com>
2015-03-30 19:51:50 -0400

    library entry points

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
 'use strict'
 
-var shojiModule = require('./lib/shoji/index'),
-    hierarchicalVariablesMod = require('./lib/hierarchical-variables/index'),
-    datasetContextMod = require('./lib/dataset-context/index'),
-    resourcesMod = require('./lib/resources/index')
-    ;
-
-var hv = hierarchicalVariablesMod(),
-    datasetContext = datasetContextMod(),
-    resources = resourcesMod(),
-    shoji = shojiModule()
-
+require('./src/crunch/index.js')()
