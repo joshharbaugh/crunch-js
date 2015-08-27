1d8a52f94fce0ebb95bb550c28885fd804286e30
Jj <jjdelc@gmail.com>
2015-08-27 14:55:20 -0500

    Convert userPreferences into a helper

--- src/user-preferences/index.js
+++ src/user-preferences/index.js
@@ -3,14 +3,10 @@
 module.exports = buildModule
 
 var angular = require('angular')
-,_ = require('lodash')
 ;
 
 function buildModule(moduleName, cfg) {
     var mod = angular.module(moduleName || 'user-preferences', []);
-    mod.factory('lodash', function() {
-        return _
-    })
     mod.factory('userPreferences', require('./user-preferences'));
     mod.factory('setUserPreferencesHandler', require('./set-user-preferences-handler'))
     return mod
--- src/user-preferences/set-user-preferences-handler.js
+++ src/user-preferences/set-user-preferences-handler.js
@@ -12,7 +12,8 @@ function setUserPreferencesHandler(bus, iResourceUser) {
         return function now(res){
             var e = {
                 event: 'preferences.saved'
-                ,preferences: command.preferences
+                ,prefName: command.prefName
+                ,value: command.value
             }
             return bus.publish(e)
         }
@@ -20,11 +21,12 @@ function setUserPreferencesHandler(bus, iResourceUser) {
     return function handle(command){
         var raise = raiseEvent(command)
         return iResourceUser.current().then(function(usr){
+            usr.preferences[command.prefName] = command.value
             usr.patch({
                 data: {
                     element: 'shoji:entity'
                     ,body: {
-                        preferences: command.preferences
+                        preferences: usr.preferences
                     }
                 }
             }).then(raise)
