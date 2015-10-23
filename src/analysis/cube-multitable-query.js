12c482101883c03700e08bd4d1a3e6742f1e78b2
Michael Malecki <mike@crunch.io>
2015-10-23 15:26:44 -0500

    fall back to magic queries when multiple response in multitable

--- src/analysis/cube-multitable-query.js
+++ src/analysis/cube-multitable-query.js
@@ -54,8 +54,12 @@ function CubeMultitableQuery(_, $q, cubeQuery){
         // switch on variable type here: either an array of queries with
         // concise magic syntax for arrays and multiple response;
         // or a proper multi table query for other types.
-
-        if (rowVariable.type === 'multiple_response' || rowVariable.type === 'categorical_array'){
+        var hasMRcolumn = _.some(multitableVariables.map(function(col){
+            return col.type === 'multiple_response'
+        }))
+        if (rowVariable.type === 'multiple_response' ||
+            rowVariable.type === 'categorical_array' ||
+            hasMRcolumn){
             var r = _.cloneDeep(rowVariable)
             var rowPart = [_.extend(r, {'dimension': 'variable', type: rowVariable.type, self: rowVariable.self})]
             if (rowVariable.type === 'categorical_array'){
